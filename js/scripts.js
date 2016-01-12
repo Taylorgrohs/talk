cf6be02341a87e92dfbe4435b4b253d1a17006f1
Jay Whang and Taylor Grohs <jaywhang83@gmail.com>
2016-01-12 08:27:56 -0800

    jQuery practice

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $("button#hello").click(function(){
+    $("ul#user").prepend("<li>hello</li>");
+    $("ul#webpage").prepend("<li>hello there</li>");
+    $("ul#user").children("li").first().click(function() {
+    $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("button#goodbye").click(function(){
+    $("ul#user").prepend("<li>goodbye</li>");
+    $("ul#webpage").prepend("<li>I said goodbye!</li>");
+    $("ul#user").children("li").first().click(function() {
+    $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+    $(this).remove();
+    });
+  });
+
+  $("button#stop").click(function(){
+    $("ul#user").prepend("<li>stop!!</li>");
+    $("ul#webpage").prepend("<li>I said stop</li>");
+    $("ul#user").children("li").first().click(function() {
+    $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+    $(this).remove();
+    });
+  });
+
+});
